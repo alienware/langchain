646687bba34d2b930bab4a4e66dfdad9287ab3c7
Tanay Upadhyaya <tanayupadhyaya@gmail.com>
2025-03-10 23:12:40 +0530

    Adds deprecated import for NestedJSONLoader

--- libs/langchain/langchain/document_loaders/__init__.py
+++ libs/langchain/langchain/document_loaders/__init__.py
@@ -105,6 +105,7 @@ if TYPE_CHECKING:
         ModernTreasuryLoader,
         MongodbLoader,
         MWDumpLoader,
+        NestedJSONLoader,
         NewsURLLoader,
         NotebookLoader,
         NotionDBLoader,
